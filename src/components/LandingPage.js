186304a0a7b92ce0eeabdc779ca3cf7335de8da8
RyanZmu <ryanzmudka@gmail.com>
2022-09-01 22:30:47 -0400

    added acnh wiki

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,6 +4,7 @@ import { Card, CardGroup, Container } from 'react-bootstrap'
 import marvel from '../images/marvel-pic.png'
 import weather from '../images/weather-pic.png'
 import jeoprady from '../images/jeoprady.png'
+import ac from '../images/ac.png'
 
 
 const LandingPage = (props) => {
@@ -33,6 +34,12 @@ const LandingPage = (props) => {
                     <Card.Title>Jeoprady Trivia Game</Card.Title>
                     <Card.Subtitle>Stack: Javascript, HTML5, CSS3 <br/> API: <a href='https://jeopardy-game-api.herokuapp.com/api-docs/'>Jeoprady-Game-API</a> </Card.Subtitle>
                     <Card.Body>Trivia game based on jeoprady. Players enter name then are given a board of random catergories each with a question for $100-$600. Users then have 30 seconds to answer the question. Reaching zero $0 means you're out. This is more of an endurance contest until I go back and add the option for a second player.</Card.Body>
+                </Card>
+                <Card>
+                    <a href='https://ryanzmu.github.io/Animal-Crossing-Wiki/index.html'><Card.Img src={ac} /></a>
+                    <Card.Title>Animal Crossing Wiki</Card.Title>
+                    <Card.Subtitle>Stack: Javascript, HTML5, CSS3 <br/> API: <a href='http://acnhapi.com/'>acnhapi</a> </Card.Subtitle>
+                    <Card.Body>Website for looking up information about items from the game Animal Crossing New Horizons. So far you can only look up fish and bugs. Very basic in its current state but will be updated to include all item catergories and villagers.</Card.Body>
                 </Card>
             </CardGroup>
         </div>
